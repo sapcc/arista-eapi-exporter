8d1adfd3266c66cf0743b5aeb6045afb059a50c3
d032408 <bernd.kuespert@sap.com>
2018-09-25 22:50:55 +0200

    new docker image version

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -1,2 +1,2 @@
-docker build . -t hub.global.cloud.sap/monsoon/arista-exporter:v0.0.6
-docker push hub.global.cloud.sap/monsoon/arista-exporter:v0.0.6
+docker build . -t hub.global.cloud.sap/monsoon/arista-exporter:v0.0.7
+docker push hub.global.cloud.sap/monsoon/arista-exporter:v0.0.7
