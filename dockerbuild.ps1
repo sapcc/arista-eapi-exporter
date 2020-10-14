17fa2db3e384c69acd955925ebf8cdf0a7d03f09
d032408 <bernd.kuespert@sap.com>
2020-10-14 14:03:00 +0200

    do not push images to hub anymore

--- dockerbuild.ps1
+++ dockerbuild.ps1
@@ -1,7 +1,4 @@
 $version = "v0.2.2"
 
-docker build . -t hub.global.cloud.sap/monsoon/arista-exporter:$version
-docker push hub.global.cloud.sap/monsoon/arista-exporter:$version
-
 docker build . -t keppel.eu-de-1.cloud.sap/ccloud/arista-exporter:$version
 docker push keppel.eu-de-1.cloud.sap/ccloud/arista-exporter:$version
