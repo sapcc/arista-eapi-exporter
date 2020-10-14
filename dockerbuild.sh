0f8ff10fb6c9ec25880250fff11b230dbf52cc17
d032408 <bernd.kuespert@sap.com>
2020-10-14 21:16:19 +0200

    bump version

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -1,4 +1,4 @@
-version="v0.2.2"
+version="v0.3.0"
 
 docker build . -t keppel.eu-de-1.cloud.sap/ccloud/arista-exporter:${version}
 docker push keppel.eu-de-1.cloud.sap/ccloud/arista-exporter:${version}
