312b6958e586d92e5f640eee73471cbcbfc67037
d032408 <bernd.kuespert@sap.com>
2020-10-14 21:15:04 +0200

    add multithreading support add route to welcome page

--- main.py
+++ main.py
@@ -1,34 +1,52 @@
-#import click
 from handler import metricHandler
+from handler import welcomePage
 import argparse
 from yamlconfig import YamlConfig
+from wsgiref.simple_server import make_server, WSGIServer, WSGIRequestHandler
 import logging
 import sys
 import falcon
 from wsgiref import simple_server
 import socket
+import threading
+from socketserver import ThreadingMixIn
 import os
 
+class _SilentHandler(WSGIRequestHandler):
+    """WSGI handler that does not log requests."""
 
-def falcon_app(port=9200, addr='0.0.0.0', exclude=list):
+    def log_message(self, format, *args):
+        """Log nothing."""
+
+class ThreadingWSGIServer(ThreadingMixIn, WSGIServer):
+    """Thread per request HTTP server."""
+    # Make worker threads "fire and forget". Beginning with Python 3.7 this
+    # prevents a memory leak because ``ThreadingMixIn`` starts to gather all
+    # non-daemon threads in a list in order to join on them at server close.
+    daemon_threads = True
+
+def falcon_app():
+    port = int(os.getenv('LISTEN_PORT', config['listen_port']))
+    exclude = config['exclude']
+    addr = '0.0.0.0'
     logging.info("Starting Arista eAPI Prometheus Server on Port %s", port)
     ip = socket.gethostbyname(socket.gethostname())
     logging.info("Listening on IP %s", ip)
     api = falcon.API()
-    api.add_route(
-        '/arista',
-        metricHandler(exclude=exclude, config=config)
-    )
+    api.add_route('/arista', metricHandler(exclude=exclude, config=config))
+    api.add_route('/', welcomePage())
+
 
     try:
-        httpd = simple_server.make_server(addr, port, api)
+        httpd = simple_server.make_server(addr, port, api, ThreadingWSGIServer)
     except Exception as e:
         logging.error("Couldn't start Server:" + str(e))
 
     try:
-        httpd.serve_forever()
+        t = threading.Thread(target=httpd.serve_forever())
+        t.daemon = True
+        t.start()
     except KeyboardInterrupt:
-        httpd.server_close()
         logging.info("Stopping Arista eAPI Prometheus Server")
 
 def enable_logging():
@@ -54,4 +72,4 @@ if __name__ == '__main__':
 
     enable_logging()
 
-    falcon_app(port=config['listen_port'], exclude=config['exclude'])
+    falcon_app()
