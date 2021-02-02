d94752f38d17c7337b61c96cf72bd347a9a52f0b
d032408 <bernd.kuespert@sap.com>
2021-02-02 10:23:10 +0100

    add keppel login

--- dockerbuild.ps1
+++ dockerbuild.ps1
@@ -1,4 +1,5 @@
-$version = "v0.3.0"
+$version = "v0.3.1"
 
 docker build . -t keppel.eu-de-1.cloud.sap/ccloud/arista-exporter:$version
+docker login keppel.eu-de-1.cloud.sap
 docker push keppel.eu-de-1.cloud.sap/ccloud/arista-exporter:$version
